18256b8c1be4b80c174af10ed6643aaa7f7f910a
Leonel Abarca <abarca.rodriguez.l@gmail.com>
2020-07-21 12:46:33 -0400

    Update FarmaService.java

--- src/main/java/com/example/restfarmanet/FarmaService.java
+++ src/main/java/com/example/restfarmanet/FarmaService.java
@@ -1,12 +1,19 @@
 package com.example.restfarmanet;
 
 import org.springframework.web.client.RestTemplate;
+import org.springframework.http.ResponseEntity;
+import java.util.List;
+import org.springframework.core.ParameterizedTypeReference;
 
 public class FarmaService {
 
-    public String getFarma() {
+    public Datos getFarma() {
         RestTemplate plantilla = new RestTemplate();
-        String resultado = plantilla.getForObject("https://farmanet.minsal.cl/maps/index.php/ws/getLocalesRegion?id_region=7", String.class);
-        return resultado;
+        // Datos resultado = plantilla.getForObject("https://farmanet.minsal.cl/maps/index.php/ws/getLocalesRegion?id_region=7", Datos);
+        ResponseEntity<List<Datos>> datosResponse = plantilla.exchange("https://farmanet.minsal.cl/maps/index.php/ws/getLocalesRegion?id_region=7",
+                     new ParameterizedTypeReference<List<Datos>>() {
+            });
+        List<Datos> datos = datosResponse.getBody();
+        return datos;
     }
 }
