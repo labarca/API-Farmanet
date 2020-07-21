87bfb4e1c55f2f3a4832efe913ffbcd9b8d4aa09
Leonel Abarca <abarca.rodriguez.l@gmail.com>
2020-07-21 11:06:01 -0400

    initial files

---
+++ src/main/java/com/example/restfarmanet/RestFarmanetApplication.java
@@ -0,0 +1,13 @@
+package com.example.restfarmanet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFarmanetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestFarmanetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/restfarmanet/RestFarmanetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restfarmanet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestFarmanetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
